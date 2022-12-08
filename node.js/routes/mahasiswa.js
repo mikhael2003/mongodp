363f862b115084b9dd0d09d855cd52430c6dcbb1
mikhael2003 <mikhaelsiman2003@gmail.com>
2022-12-08 08:23:09 +0700

    update mahasiswa.js

--- node.js/routes/mahasiswa.js
+++ node.js/routes/mahasiswa.js
@@ -7,8 +7,24 @@ router.post('/', async(req, res) => {
         nama: req.body.nama, 
         alamat: req.body.alamat
     })
+})
 
+// Update (method PUT)
+router.put('/:mahasiswaId', async(req,res) => {
+    //taqmpang data yang mau diubah
+    const data = {
+        nama: req, body,nama,
+        alamat: req,body,alaamat
+    }
 
-
-    
-} )
+    try {
+        //update data disini
+        const mahasiswa = awaitMahasiswa.UpdateOne({
+            _id: req.params.mahasiswaId
+        }, data)
+        //response sucsess
+        res.json(mahasiswa)
+       } catch (error) {
+           res.json({message: error})
+       }
+})
